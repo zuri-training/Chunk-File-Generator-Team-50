2f2dfaee07f7594c829203f2b5a7deee3742dd7c
Ezekiel Okebule <jamesezekiel039@gmail.com>
2022-08-09 04:47:40 +0100

    linked frontend to django

--- Backend/chunkfile/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'accounts'
-urlpatterns = [
-    path("login/", views.loginPage, name="login"),
-    path("logout/", views.logoutUser, name="logout"),
-    path("register/", views.registerPage, name='register')
-]
