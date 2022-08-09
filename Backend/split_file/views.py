b8a2a75b1b9daf77ead47cd136c5bd920009275e
Tersoo <tersooahire21@gmail.com>
2022-08-09 10:05:52 +0100

    Made changes to user authentication and added logout link on tools page

--- Backend/split_file/views.py
+++ Backend/split_file/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url="accounts:register")
+@login_required(login_url="accounts:login")
 def split_home(request):
     return render(request, 'split_file/home.html')
 
-@login_required(login_url="accounts:register")
+@login_required(login_url="accounts:login")
 def split_csv(request):
     return render(request, 'split_file/splitcsv.html')
 
-@login_required(login_url="accounts:register")
+@login_required(login_url="accounts:login")
 def split_json(request):
     return render(request, 'split_file/split_json.html')
