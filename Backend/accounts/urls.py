3dd4bf995d3a13e5d9a79bf038c137599467a817
carnage999-max <105145697+carnage999-max@users.noreply.github.com>
2022-08-12 07:28:57 +0100

    Update urls.py

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
+    path("profile/", views.dashboard, name="profile"),
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutUser, name="logout"),
     path("register/", views.registerPage, name='register')
